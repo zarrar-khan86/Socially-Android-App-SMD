1e27ef15b55b0ec792756942d79ea1a421f9614e
zarrar-khan86 <i221203@gmail.com>
2025-09-18 01:51:31 +0500

    InboxPage

--- app/src/main/java/com/zarrar/i221203/InboxActivity.kt
+++ app/src/main/java/com/zarrar/i221203/InboxActivity.kt
@@ -1,36 +1,28 @@
 package com.zarrar.i221203
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomePageActivity : AppCompatActivity() {
+class InboxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page)
 
+        setContentView(R.layout.activity_inbox)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val ivSearchBtn = findViewById<ImageView>(R.id.ivSearchBtn)
-        val ivMessages = findViewById<ImageView>(R.id.ivMessages)
-
-        ivMessages.setOnClickListener {
-            val intent = Intent(this, InboxActivity::class.java)
-            startActivity(intent)
-        }
-
-        ivSearchBtn.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
+        val ivMessageBack = findViewById<ImageView>(R.id.ivMessageBack)
+        ivMessageBack.setOnClickListener {
+            val intent = Intent(this, HomePageActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
